3dfd29c49e244f8dea4c6571801e67953fa56453
Manzi <manzeyjoseph@gmail.com>
2020-12-21 14:09:35 +0000

    just changed few

--- app/src/main/java/com/manzi/myapplication/MainActivity.kt
+++ app/src/main/java/com/manzi/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.manzi.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
